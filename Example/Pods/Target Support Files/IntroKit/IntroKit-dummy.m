33a263034148db2b4342f44c6f82afeb7da7f1f6
Sumit Mukhija <sumitmukhija@hotmail.com>
2017-04-21 11:51:04 +0530

    Initial commit

---
+++ Example/Pods/Target Support Files/IntroKit/IntroKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IntroKit : NSObject
+@end
+@implementation PodsDummy_IntroKit
+@end
---
+++ Example/Pods/Target Support Files/IntroKit/IntroKit-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double IntroKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char IntroKitVersionString[];
+
